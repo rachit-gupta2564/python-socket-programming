44f1cd0e89429561dc84cb471885fbfcea69efde
Rachit Gupta <guptarachit085@gmail.com>
2024-04-24 15:13:38 +0530

    CHATTING APP

--- chatting_app_server.py
+++ chatting_app_server.py
@@ -1,19 +1,19 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-address = ('localhost',1234)
-#address = (socket.gethostbyname(), 1234)
+address = (socket.gethostname(), 1234)
 s.bind(address)
 s.listen(1)
 print("Waiting for connection...", address)
+client, addr = s.accept()
+print("Connections established", addr)
 while 1:
-    client, addr = s.accept()
-    print("Connected by", addr)
     data = client.recv(1024)
     if not data:
-        s.close()
         break
     print("Client:", data)
+    data = input('Server:').encode()
     client.send(data)
-    print("Echo send")
-    client.close()
+
+client.close()
+s.close()
